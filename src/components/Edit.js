9023f083903c8a11dd648b7fbcce4d468362fde3
Rose <rose.wan@rci.rogers.com>
2019-08-28 12:28:05 -0400

    added item number to the Edit page

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -2,17 +2,25 @@
 
 import React, { Component } from 'react';
 
-import Home from './Home.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class Edit extends Component {
   render() {
     return (
+      <Router>
         <div>
           <h2>Edit</h2>
-          <p>Item is </p>
+          <p>Item is <Route path="/edit/:id" component={Contact} /></p>
         </div>
+      </Router>
     );
   }
+}
+
+function Contact({ match }) {
+  return (
+    <span>{match.params.id}</span>
+  );
 }
 
 export default Edit;
