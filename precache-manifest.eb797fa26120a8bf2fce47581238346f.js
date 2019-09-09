3519878e495b230bb806e9803b7b4a5410dc96ee
Rose <rose.wan@rci.rogers.com>
2019-09-09 16:43:57 -0400

    Updates

---
+++ precache-manifest.eb797fa26120a8bf2fce47581238346f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d836a14cbe06a53b6548ad67ce4eb593",
+    "url": "/spa-project-with-lists/index.html"
+  },
+  {
+    "revision": "bcdb814300b1638c5339",
+    "url": "/spa-project-with-lists/static/css/main.e86e53b7.chunk.css"
+  },
+  {
+    "revision": "8e5e754560ad859724c0",
+    "url": "/spa-project-with-lists/static/js/2.521681e4.chunk.js"
+  },
+  {
+    "revision": "bcdb814300b1638c5339",
+    "url": "/spa-project-with-lists/static/js/main.76fab3eb.chunk.js"
+  },
+  {
+    "revision": "8f091a7a7046431692c8",
+    "url": "/spa-project-with-lists/static/js/runtime~main.caaa7ceb.js"
+  }
+]);
